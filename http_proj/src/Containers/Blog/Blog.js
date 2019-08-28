181623d15ec81475a7938818fedbe9f2045b765b
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-08-28 02:22:45 -0700

    Axios handling errors

--- http_proj/src/Containers/Blog/Blog.js
+++ http_proj/src/Containers/Blog/Blog.js
@@ -10,7 +10,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -26,6 +27,9 @@ class Blog extends Component {
                 });
 
                 this.setState({posts: updatedPosts});
+            })
+            .catch(error => {
+                this.setState({error: true});
             });
     }
 
@@ -35,13 +39,18 @@ class Blog extends Component {
 
     render () {
 
-        const posts = this.state.posts.map( post => {
-            return <Post 
-                        key={post.id}
-                        title={post.title} 
-                        author={post.author}
-                        clicked={() => this.postClickedHandler(post.id)}/>
-        })
+        let posts = <p style={{textAlign: 'center'}}>Something Went Wrong</p>
+
+        if(!this.state.error){
+            posts = this.state.posts.map( post => {
+                return <Post 
+                            key={post.id}
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postClickedHandler(post.id)}/>
+            });
+        }
+    
 
         return (
             <div>
