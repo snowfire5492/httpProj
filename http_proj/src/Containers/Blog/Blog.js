d219996995837410c5e62118dbe0e27f18135635
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-08-28 00:24:17 -0700

    Make post clickable

--- http_proj/src/Containers/Blog/Blog.js
+++ http_proj/src/Containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -28,13 +29,18 @@ class Blog extends Component {
             });
     }
 
+    postClickedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
 
         const posts = this.state.posts.map( post => {
             return <Post 
                         key={post.id}
                         title={post.title} 
-                        author={post.author}/>
+                        author={post.author}
+                        clicked={() => this.postClickedHandler(post.id)}/>
         })
 
         return (
@@ -43,7 +49,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
