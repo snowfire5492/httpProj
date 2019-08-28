0dbd64a4c9dae53203cb2e20f8ce375bb0111f25
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-08-28 00:14:53 -0700

    transforming incoming data

--- http_proj/src/Containers/Blog/Blog.js
+++ http_proj/src/Containers/Blog/Blog.js
@@ -15,7 +15,16 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts/')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0,4);
+
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+
+                this.setState({posts: updatedPosts});
             });
     }
 
@@ -24,7 +33,8 @@ class Blog extends Component {
         const posts = this.state.posts.map( post => {
             return <Post 
                         key={post.id}
-                        title={post.title} />
+                        title={post.title} 
+                        author={post.author}/>
         })
 
         return (
