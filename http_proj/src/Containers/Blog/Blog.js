2272070c9af551be63c8489c9744975a33fbcd69
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-09-17 02:36:07 -0700

    adding NavLink

--- http_proj/src/Containers/Blog/Blog.js
+++ http_proj/src/Containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import Posts from './Posts/Posts';
 
 import './Blog.css';
@@ -18,12 +18,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink to="/">Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
