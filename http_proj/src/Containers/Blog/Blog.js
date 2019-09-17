ecdc6f1d2ad8c082cad2392fe62f67d447559922
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-09-17 03:32:41 -0700

    routing

--- http_proj/src/Containers/Blog/Blog.js
+++ http_proj/src/Containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import FullPost from './FullPost/FullPost';
 
@@ -30,8 +30,10 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
