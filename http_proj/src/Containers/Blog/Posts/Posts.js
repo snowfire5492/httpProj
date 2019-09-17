5d75a6baa4ea51e191c3265bb5e5cc01eb46fc7e
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-09-17 01:38:15 -0700

    Adding routing

--- http_proj/src/Containers/Blog/Posts/Posts.js
+++ http_proj/src/Containers/Blog/Posts/Posts.js
@@ -8,6 +8,7 @@ class Posts extends Component {
         posts: []
     }
     componentDidMount() {
+        console.log(this.props);
         axios.get('/posts/')
             .then(response => {
                 const posts = response.data.slice(0,4);
